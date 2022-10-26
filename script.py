b0774387b6dff226e72893f028c245bec2e9176f
allialy10 <l.allialy@epsi.fr>
2022-10-26 14:21:14 +0200

    Création et Lecture d'un csv

--- script.py
+++ script.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 donnees = pd.read_csv('conso-annuelles_v1.csv',sep =';', encoding ='Latin-1')
 
-print (donnees)
+donnees.to_csv(f'conso-clean.csv',index =False, header = True)
+
+donnees1 = pd.read_csv('conso-clean.csv',sep =';', encoding ='Latin-1')
+
+print(donnees1)
