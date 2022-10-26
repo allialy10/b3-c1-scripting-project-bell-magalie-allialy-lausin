fae6ba90a92ba2ddfb4b9c3f0d70451433b4ea38
allialy10 <l.allialy@epsi.fr>
2022-10-26 15:40:02 +0200

    Création et Lecture d'un csv

--- script.py
+++ script.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
+import numpy as np
+
+# Lecture d'un ficher 
+
+
 donnees = pd.read_csv('conso-annuelles_v1.csv',sep =';', encoding ='Latin-1')
 
-donnees.to_csv(f'conso-clean.csv',index =False, header = True)
+# Création d'un fichier csv à partir du csv original
+
+donnees.to_csv(f'conso-clean.csv',index =False, sep= ';', header = True, na_rep="")
 
 donnees1 = pd.read_csv('conso-clean.csv',sep =';', encoding ='Latin-1')
 
-print(donnees1)
+
+print((donnees1))
