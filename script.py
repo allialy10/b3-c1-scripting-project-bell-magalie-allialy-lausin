73cce5e68887ccd0b3cd12da4651c181bc78d3cb
allialy10 <l.allialy@epsi.fr>
2022-11-01 04:26:29 +0100

    traitement des données

--- script.py
+++ script.py
@@ -1,17 +1,43 @@
-import pandas as pd
+import csv
 
-import numpy as np
+#Lecture du CSV
 
-# Lecture d'un ficher 
-
-
-donnees = pd.read_csv('conso-annuelles_v1.csv',sep =';', encoding ='Latin-1')
-
-# Création d'un fichier csv à partir du csv original
-
-donnees.to_csv(f'conso-clean.csv',index =False, sep= ';', header = True, na_rep="")
-
-donnees1 = pd.read_csv('conso-clean.csv',sep =';', encoding ='Latin-1')
+with open ("conso-annuelles_v1.csv", encoding = 'Latin-1') as f:
 
 
-print((donnees1))
+    data = csv.reader(f, delimiter=';')
+    count = 0
+
+
+    #Ouverture et création du fichier conso-clean en mode ecriture
+
+    with open ('conso-clean.csv','w', encoding= 'Latin-1', newline='') as clean :
+
+        #Création de la ligne d'entête
+        
+        fieldnames=['Appareil suivi','Consommation annuelle AN1','Consommation annuelle AN2','Type', 'Consommation globale']
+        
+        writer= csv.DictWriter(clean, fieldnames=fieldnames)
+        writer.writeheader()
+        
+
+        for ligne in data:
+
+            #Exclusion des lignes contenants des cellules vides
+            if (ligne[0] != '' and ligne[1] != '' and ligne[2] != '' and ligne[3] != '' and ligne[4] != '' and ligne[3]!= '-' and count > 0) :
+                
+                #Conversion des consommations pour pouvoir les additionner
+                ligne[2]= float(ligne[2].replace(',','.'))
+                ligne[3]= float(ligne[3].replace(',','.'))
+
+
+                #Addition des consommations
+                e = format(ligne[2] + ligne[3], '.1f')
+                
+
+                #ecriture des lignes dans le fichier conso-clean
+                writer.writerow({'Appareil suivi': ligne[0],'Consommation annuelle AN1' : ligne[2],'Consommation annuelle AN2': ligne[3],'Type' : ligne[4], 'Consommation globale' : e})
+        
+            count+=1
+        
+            
